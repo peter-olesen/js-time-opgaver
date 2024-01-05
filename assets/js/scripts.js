08f4e49ad5cac5e068550c3c70b85c0601f2dfcc
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-01-05 09:56:23 +0100

    i did a thing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,48 @@
-// Write cool JavaScript below
+const formContainer = document.getElementById('form-container');
+
+const firstnameInput = document.getElementById('firstname');
+const lastnameInput = document.getElementById('lastname');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const repeatpasswordInput = document.getElementById('repeatpassword');
+const submitButton = document.getElementById('btn-submit');
+
+submitButton.addEventListener('click', (e) => {
+
+    // Prevents submit button default action
+    e.preventDefault();
+    
+    // Clear console on submit for better console validation results
+    console.clear();
+
+    let submittedFirstname = firstnameInput.value.trim();
+    let submittedLastname = lastnameInput.value.trim();
+    let submittedPassword = passwordInput.value.trim();
+    let submittedRepeatpassword = repeatpasswordInput.value.trim();
+
+    if (
+        submittedFirstname.length > 2 &&
+        submittedLastname.length > 2 &&
+        validateEmail(emailInput.value) &&
+        submittedPassword.length > 6 &&
+        submittedRepeatpassword.length > 6 &&
+        submittedPassword === submittedRepeatpassword
+    ) {
+        console.log ('Everything looks good')
+    }
+
+    else (
+        console.log ('You broke something, FIX IT!')
+    )
+});
+
+function validateEmail(emailInput) {
+    // Regular expression for email
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Test the email against the pattern
+    let isValid = emailRegex.test(emailInput);
+
+    // Return results of validation (true eller false)
+    return isValid;
+}
